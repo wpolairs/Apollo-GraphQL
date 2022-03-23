4a6057e538ca1e98ab4a4adb5bae624e8490c10a
Wang Jian <jian.wang@tripalink.com>
2022-03-23 17:07:59 +0800

    feat: 基础知识

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react"
+import { Layout, QueryResult } from "../components"
+import TrackCard from "../containers/track-card"
+import { gql, useQuery } from "@apollo/client"
+
+const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
-};
+  const { loading, error, data } = useQuery(TRACKS)
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track, index) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  )
+}
 
-export default Tracks;
+export default Tracks
